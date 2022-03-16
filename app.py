866ce45daabf3947d64774977677f4bfa10602fa
GayathriN <gayathrin1510@gmail.com>
2022-03-16 22:43:53 +0530

    Added all my changes - flask appln

--- app.py
+++ app.py
@@ -1,61 +1,22 @@
+## Date: 12th March 2022
+## Author: GAYATHRI N
+# SENTIMENT BASED PRODUCT RECOMMENDATION SYSTEM
 
+import model
 from flask import Flask, redirect, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow())
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue adding your task"
+        user = str(request.form['username'])
+        productRecommendations = model.get_sentimentBasedProductRecommendations(user)
+        return render_template('index.html', products =productRecommendations, users=model.UserList)
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+        return render_template('index.html', products =[], users=model.UserList)
 
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return "There was a problem deleting that task"
-
-@app.route('/update/<int:id>', methods=['GET','POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "unable to update task"
-    else:
-        return render_template('update.html', task=task)
-
+# Run application
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,81 @@
+# AUTHOR: GAYATHRI N
+# DATE: 12- mARCH-2022
+# Version: 1.5
+# Importing required libraries
+
+import pandas as pd
+import numpy as np
+import pickle
+
+# Importing common_utils for loading required variables
+import common_utils
+
+
+# Loading Recommendation System
+ProductRecommendationSystem = pickle.load(file=open(common_utils.recomm_system_path, 'rb'))
+# Loading ProductReviewMapping (TRAIN)
+productMap = pd.read_csv(common_utils.prod_reviews_mapping_path)
+productReviewsData = pd.read_csv(common_utils.processed_train_data_path)
+# Loading Vectorizer
+vectorizer = pickle.load(file=open(common_utils.tfidf_vectorizer_path, 'rb'))
+# Loading Sentiment Classifier
+SentimentClassifier = pickle.load(file=open(common_utils.sentiment_classifier_path, 'rb'))
+# Loading the Probability Threshold
+probThreshold = common_utils.prob_threshold_val
+
+userList = list(ProductRecommendationSystem.index)
+
+recommendationProductSentimentMap = {}
+
+
+# Defining function for Sentiment based Product Recommendations
+def get_sentimentBasedProductRecommendations(username):
+    '''
+    This function takes username as input and
+    generates top 5 product recommendations based on their buying preferences.
+    This further enhances the recommendations based on overall positive reviews for the products 
+    -> Uses Item-based Recommendation System::
+    '''
+ 
+    # Fetching top 20 recommendations for user
+    try:
+        if len(username) < 1:
+            raise KeyError
+        # fetch the top 20 recommendations for the user
+        topRecommendations_id = ProductRecommendationSystem.loc[username].sort_values(ascending=False)[:20]
+        topRecommendations = pd.merge(left=productMap, right=topRecommendations_id, on='id')
+        
+    except KeyError:
+        # If user doesn't exist print the error message
+        errorMessage = "ERROR: Unable to find user '{}' in our database!\n\
+            Please try again with a user from 'Available Users' list provided above. ".format(username)
+        
+        return errorMessage
+    
+    # Creating a Dictionary to store the percentage of positive sentiments
+    recommendationsPositivePercentage = {}
+    
+    # Iterating over all the recommendations
+    for product in topRecommendations.name:
+
+        # extracting the reviews for every recommended product
+        filteredReviews = (productReviewsData[productReviewsData.name == product].reviews_text)
+
+        #pre_process and vectorize the text
+        filteredReviews = filteredReviews.apply(pre_process_text)
+        reviews_vectorized = vectorizer.transform(filteredReviews)
+
+        # get the average sentiment for every product
+        sentimentProbs = SentimentClassifier.predict_proba(reviews_vectorized).T[1]
+        reviewsSentiment = [1 if sentiment>0.55 else 0 for sentiment in sentimentProbs]
+        positvePercent = sum(reviewsSentiment)/len(reviewsSentiment)
+
+        recommendationProductSentimentMap[product] = positvePercent
+
+    # sort the dict with the decreasing order of average positive reviews
+    recommendationProductSentimentMap_sorted = dict(sorted(recommendationProductSentimentMap.items(), key = lambda x: x[1], reverse = True))
+
+    # retrieve the top 5 products based on positve sentiment
+    finalRecommendations = list(recommendationProductSentimentMap_sorted.keys())[:5]
+    
+    return finalRecommendations
